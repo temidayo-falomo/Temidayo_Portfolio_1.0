8fda8fee36f159b9a0f36f44b72033a8dbc49be9
temidayo-falomo <temmideee@gmail.com>
2022-05-16 16:51:25 +0100

    updated routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import About from "./pages/about/About";
-import Home from "./pages/home/Home";
-import Projects from "./pages/projects/Projects";
-import Contact from "./pages/contact/Contact";
+import ErrorPage from "./components/errorPage/ErrorPage";
+import RoutesContainer from "./RoutesContainer";
 
 function App() {
   return (
-    <Router>
-      <>
-        <Routes>
-          <Route exact path="/home" element={<Home />} />
-          <Route path="projects" element={<Projects />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact-me" element={<Contact />} />
-        </Routes>
-      </>
-    </Router>
+    <>
+      <RoutesContainer />
+      <ErrorPage />
+    </>
   );
 }
 
---
+++ src/components/errorPage/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './ErrorPage.css'
+
+function ErrorPage() {
+  return (
+    <div className='error-page'>
+        <h1>Oops! It looks Like This Link is Unavailable!</h1>
+        <a href='/home'><button>Back To Home Page</button></a>
+    </div>
+  )
+}
+
+export default ErrorPage
