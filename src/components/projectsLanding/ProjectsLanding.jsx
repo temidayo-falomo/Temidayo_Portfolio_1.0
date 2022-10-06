b40418004cda717297454ca87864dd722e25476e
temidayo-falomo <temmideee@gmail.com>
2022-10-06 20:32:21 +0100

    new project

--- src/components/projectsLanding/ProjectsLanding.jsx
+++ src/components/projectsLanding/ProjectsLanding.jsx
@@ -7,21 +7,21 @@ function ProjectsLanding() {
   return (
     <div className="projectsLanding">
       <div className="projects-left">
-        <h1 style={{marginBottom: '2rem', color: '#43d9ad'}}>Github Links</h1>
+        <h1 style={{ marginBottom: "2rem", color: "#43d9ad" }}>Github Links</h1>
         <ul>
-          {projectList.map((proj) => {
+          {projectList.map((proj, index) => {
             return (
-              <li key={proj.name}>
+              <li key={index}>
                 <a href="#">{proj.name}</a>
               </li>
             );
           })}
         </ul>
       </div>
-      
+
       <div className="projects-right">
-        {projectData.map((data)=> {
-          return <ProjectCard key={data.id} data={data}/>
+        {projectData.map((data) => {
+          return <ProjectCard key={data.id} data={data} />;
         })}
       </div>
     </div>
--- src/components/projectsLanding/projectsData.js
+++ src/components/projectsLanding/projectsData.js
@@ -1,6 +1,5 @@
 export const projectData = [
   {
-    id: "10",
     name: "Insttagg",
     img: "./assets/Projects Screenshots/insttagg.jpg",
     desc: `A Fullstack re-design of 
@@ -10,14 +9,14 @@ export const projectData = [
   },
 
   {
-    id: "2",
-    name: "Bithub",
-    img: "./assets/Projects Screenshots/mini-github.jpg",
-    desc: "I built Github, but as Bithub using Github's official REST Api/localStorage to retrive users' info.",
-    link: "https://bit-hub.netlify.app/",
+    name: "WhatsApp Mini",
+    img: "./assets/Projects Screenshots/whatsapp.jpg",
+    desc: `I built a Fullstack re-design of WhatsApp using the most
+    creative/ effecient solutions i could provide, it performs Realtime Chat, CRUD operations, e.t.c`,
+    link: "https://whatsapp-mini.netlify.app",
   },
+
   {
-    id: "1",
     name: "Booksy",
     img: "./assets/Projects Screenshots/booksyy.jpg",
     desc: `I Built Booksy as a special personal side-project, 
@@ -25,22 +24,26 @@ export const projectData = [
     functionalities using a BAAS(Firebase).`,
     link: "https://booksyy.netlify.app",
   },
+
   {
-    id: "3",
+    name: "Bithub",
+    img: "./assets/Projects Screenshots/mini-github.jpg",
+    desc: "I built Github, but as Bithub using Github's official REST Api/localStorage to retrive users' info.",
+    link: "https://bit-hub.netlify.app/",
+  },
+  {
     name: "Burger-A",
     img: "./assets/Projects Screenshots/burger-a-proj.png",
     desc: "A web app that allows users to build their custom Burger & Checkout.",
     link: "https://burger-a.netlify.app/make-burger",
   },
   {
-    id: "4",
     name: "Weather App",
     img: "./assets/Projects Screenshots/weather-app.jpg",
     desc: "Created my First Weather App!!(With ReactJs)Everybody should have at least one weather app in my opinion.",
     link: "https://what-is-the-weather-proj.netlify.app/",
   },
   {
-    id: "5",
     name: "Coin Fi",
     img: "./assets/Projects Screenshots/coin-fi.jpg",
     desc: "I've wanted to build a Crypto price tracker since i learned about API's(Consumed the Coin Gecko Public API)",
@@ -48,14 +51,12 @@ export const projectData = [
     env: "FaReact",
   },
   {
-    id: "6",
     name: "Tiny Moviez",
     img: "./assets/Projects Screenshots/tiny-moviez.jpg",
     desc: "Built an API powered Movie search app using The Movie database.",
     link: "https://tiny-movies-temidayo.netlify.app/",
   },
   {
-    id: "7",
     name: "My Portfolio",
     img: "./assets/Projects Screenshots/myportfolio.jpg",
     desc: `My portfolio website, 
@@ -64,14 +65,12 @@ export const projectData = [
     link: "https://temidayo-falomo.netlify.app/",
   },
   {
-    id: "8",
     name: "Panda Nft",
     img: "./assets/Projects Screenshots/panda-nft.jpg",
     desc: "I built a NFT landing page using mainly inbuilt css flexbox-properties for Responsiveness.",
     link: "https://panda-nfts.netlify.app/",
   },
   {
-    id: "9",
     name: "SaturnFi(Live)",
     img: "./assets/Projects Screenshots/saturn-fi.jpg",
     desc: "Did a freelance project on a contract for a Blockchain startup.",
