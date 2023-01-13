57f080c8dd88f663610ac10bd579dcaa4b80d7ec
temidayo <temmideee@gmail.com>
2023-01-13 11:12:21 +0100

    ui updates

--- src/components/projectsLanding/ProjectCard.jsx
+++ src/components/projectsLanding/ProjectCard.jsx
@@ -3,7 +3,16 @@ import React from "react";
 function ProjectCard({ data }) {
   return (
     <a href={data.link} target="_blank" rel="noopener noreferrer">
-      <div className="card-top">
+      <div
+        className="card-top"
+        // style={{
+        //   backgroundImage: `url(${
+        //     require("./assets/Projects Screenshots/insttagg.jpg").default
+        //   })`,
+        //   backgroundPosition: "center",
+        //   backgroundSize: "cover",
+        // }}
+      >
         <img src={data.img} alt="" loading="lazy" />
       </div>
       <div className="card-desc">
